d267c52c11870451d624beecb029f86bd5643590
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2021-10-18 19:09:57 +0200

    CLOCK/CMSIS

---
+++ TP/clocks.h
@@ -0,0 +1,6 @@
+#ifndef CLOCKS_H
+#define CLOCKS_H
+
+void clocks_init(void);
+
+#endif
--- TP/led.c
+++ TP/led.c
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include "led.h"
 
+#define STM32L475xx //c'est où se trouve le typedef GPIO
+#include "stm32l4xx.h"  //on doit incluire le fichier pour l'utiliser
+
+/*
 #define RCC_AHB2ENR (*(volatile uint32_t *) (0x40021000 + 0X4C)) //page 83 de manual
 #define GPIOB_MODER (*(volatile uint32_t *) (0x48000400))
 #define GPIOB_BSSR (*(volatile uint32_t *) (0x48000400 + 0x18))
@@ -9,20 +13,21 @@
 #define RCC_AHB2ENR_C (*(volatile uint32_t *) (0x40021000 + 0X4C))
 #define GPIOC_MODER (*(volatile uint32_t *) (0x48000800)) //page 82 de manual
 #define GPIOC_BSSR (*(volatile uint32_t *) (0x48000800 + 0x18))//page 82 de manual
+*/
 
 void led_init(){
     //Pour activer l'horloge du port B dans registre idoine
   //  uint32_t v= RCC_AHB2ENR;
   //  v= v | (1<<1);
   //  RCC_AHB2ENR=v;
-    RCC_AHB2ENR |= (1<<1);
+    RCC->AHB2ENR |= (1<<1);
 
     //Pour configurer la broche PB14 en sortie
-    GPIOB_MODER |= (1<<28);
-    GPIOB_MODER &= ~ (1<<29);
+    GPIOB->MODER |= (1<<28);
+    GPIOB->MODER &= ~ (1<<29);
 
     //LED3 YELLOW - LED 4 BLUE du port c
-    RCC_AHB2ENR_C |= (1<<2);
+    RCC->AHB2ENR |= (1<<2);
 
     //Pour configurer la vitesse (11 very high speed), c'est pas trés necessaire
 //    GPI0B_OSPEEDR |=(1<<28);
@@ -30,32 +35,32 @@ void led_init(){
 }
 
 void led_g_on(){
-    GPIOB_BSSR = (1<<14); //activer position 14
+    GPIOB->BSRR = (1<<14); //activer position 14
 
 }
 
 void led_g_off(){
-    GPIOB_BSSR = (1<<14+ 16); // 14+16=30 pour activer le reset de 14
+    GPIOB->BSRR = (1<<14+ 16); // 14+16=30 pour activer le reset de 14
 
 }
 
 void led(state stateled ){ //sateled pour changer de state LED_OFF , LED_YELLOW, LED_BLUE
     if( stateled == LED_OFF)
     {
-        GPIOC_MODER &= ~(1<<18);
-        GPIOC_MODER &= ~(1<<19);//configurer en entrée
+        GPIOC->MODER &= ~(1<<18);
+        GPIOC->MODER &= ~(1<<19);//configurer en entrée
     }else{
         if (stateled == LED_YELLOW)
         {
-            GPIOC_MODER |= (1<<18);
-            GPIOC_MODER &= ~(1<<19);//configurer en sortie
-            GPIOC_BSSR = (1<<9);//activer position 9 (set)
+            GPIOC->MODER |= (1<<18);
+            GPIOC->MODER &= ~(1<<19);//configurer en sortie
+            GPIOC->BSRR = (1<<9);//activer position 9 (set)
         }else{
             if (stateled == LED_BLUE)
             {
-                GPIOC_MODER |= (1<<18);
-                GPIOC_MODER &= ~(1<<19);//configurer en sortie
-                GPIOC_BSSR = (1<<25);//activer position 9+16 (reset)
+                GPIOC->MODER |= (1<<18);
+                GPIOC->MODER &= ~(1<<19);//configurer en sortie
+                GPIOC->BSRR = (1<<25);//activer position 9+16 (reset)
             }
         }
     }
